84444721da3fea3212b9e634f9fab6fe84d28300
slatermatt <matthewslater1998@gmail.com>
2022-01-31 00:54:17 +0000

    Adds news & articles

---
+++ src/api/news-page/controllers/news-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  news-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::news-page.news-page');
---
+++ src/api/news-page/routes/news-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-page.news-page');
